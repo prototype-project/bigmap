baf1efa11ab9f226145a667e1d6b2c64b156f5a8
Bartlomiej Beczkowski <bartlomiej.beczkowski@allegro.pl>
2018-06-07 21:54:58 +0200

    deploy script

--- deploy.py
+++ deploy.py
@@ -5,12 +5,22 @@ import requests as req
 '''
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
+docker network rm $(docker network ls)
 '''
 
 class Instance:
     def __init__(self, container, address):
         self.container = container
         self.address = address
+
+def get_instance_network_address(client, network_name, instance):
+    network = client.networks.get(network_name)
+    for pk, c in network.attrs['Containers'].items():
+        if c['Name'] == instance.container.name:
+            return f"http://{c['IPv4Address'].split('/')[0]}"
+    else:
+        raise RuntimeException('cannot find instance in network')
+
 
 def healthy(port):
     try:
@@ -22,11 +32,12 @@ def healthy(port):
 def create_network(client, network_name):
     return client.networks.create(network_name, driver='bridge')
 
-def create_instances(client, number_of_instances, port_start):
+def create_instances(client, number_of_instances, port_start, network_name):
     replicas = [Instance(
         client.containers.run(
             "beczkowb/io.bigmap",
             detach=True,
+            network=network_name,
             ports={'8080/tcp': port_start + i}),
         f'http://localhost:{port_start + i}'
     ) for i in range(number_of_instances)]
@@ -41,12 +52,14 @@ def configure_replicas(replicas, port_start):
         req.put(
             f'http://localhost:{port_start + i}/map/admin/set-as-replica')
 
-def connect_instances_to_network(network, instances):
-    for i in instances:
-        network.connect(i.container)
+# def connect_instances_to_network(network, instances):
+#     for i in instances:
+#         network.connect(i.container)
+#     time.sleep(10)
 
-def configure_master(master, replicas):
-    req.put(f'{master.address}/map/admin/set-as-master', json=[r.address for r in replicas])
+def configure_master(client, master, replicas, network_name):
+    req.put(f'{master.address}/map/admin/set-as-master',
+            json=[get_instance_network_address(client, network_name, r) for r in replicas])
 
 def deploy():
     CONFIG = {
@@ -63,17 +76,17 @@ def deploy():
 
     for master_i in range(CONFIG['number_of_masters']):
         port = START_PORT + master_i * CONFIG['number_of_replicas']
-        replicas = create_instances(CLIENT, CONFIG['number_of_replicas'], port)
+        replicas = create_instances(CLIENT, CONFIG['number_of_replicas'], port, CONFIG['network_name'])
         all_replicas.extend(replicas)
         configure_replicas(replicas, port)
-        connect_instances_to_network(network, replicas)
+        # connect_instances_to_network(network, replicas)
 
     replicas_per_master = [all_replicas[i: i + CONFIG['number_of_replicas']] for i in range(0, len(all_replicas), CONFIG['number_of_replicas'])]
     for master_i, replicas_for_master in enumerate(replicas_per_master):
         port = START_PORT + CONFIG['number_of_masters'] * CONFIG['number_of_replicas'] + master_i
-        master = create_instances(CLIENT, 1, port)[0]
-        connect_instances_to_network(network, [master])
-        configure_master(master, replicas_for_master)
+        master = create_instances(CLIENT, 1, port, CONFIG['network_name'])[0]
+        # connect_instances_to_network(network, [master])
+        configure_master(CLIENT, master, replicas_for_master, CONFIG['network_name'])
 
 if __name__ == '__main__':
     deploy()
--- src/main/java/io/bigmap/store/infrastructure/AsyncHttpReplicaNotifier.java
+++ src/main/java/io/bigmap/store/infrastructure/AsyncHttpReplicaNotifier.java
@@ -2,10 +2,14 @@ package io.bigmap.store.infrastructure;
 
 import io.bigmap.store.domain.ReplicaNotifier;
 import io.bigmap.store.domain.StoreSetup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.client.AsyncRestTemplate;
 
 public class AsyncHttpReplicaNotifier implements ReplicaNotifier {
+
+    private static final Logger log = LoggerFactory.getLogger(AsyncHttpReplicaNotifier.class);
 
     private final AsyncRestTemplate restTemplate;
     private final StoreSetup storeSetup;
@@ -26,13 +30,16 @@ public class AsyncHttpReplicaNotifier implements ReplicaNotifier {
                 .forEach(r -> restTemplate.put(r + "/map/" + key, new HttpEntity<>(value))
                         .addCallback(
                                 result -> infrastructureMetrics.notifySuccessCounter().increment(),
-                                ex -> infrastructureMetrics.notifyFailureCounter().increment()));
+                                ex -> {
+                                    infrastructureMetrics.notifyFailureCounter().increment();
+                                    log.error(ex.getMessage());
+                                }));
     }
 
     @Override
     public void notifyReplicasOnDelete(String key) {
         storeSetup.getReplicas().forEach(r ->
-                restTemplate.delete(r + "/map" + key).addCallback(result ->
+                restTemplate.delete(r + "/map/" + key).addCallback(result ->
                                 infrastructureMetrics.notifySuccessCounter().increment(),
                         ex -> infrastructureMetrics.notifyFailureCounter().increment()));
     }
